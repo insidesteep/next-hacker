0dec3c3d858fbff98867d72fe1ffd0b2009d52b4
Salaxiddinov Suxrob <salaxiddinovsuxrob@gamil.com>
2020-06-17 13:31:03 +0400

    Add Layout component

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,56 @@
 import Error from 'next/error'
+import Link from 'next/link'
+import Layout from '../components/Layout'
 import StoryList from '../components/StoryList'
 
-const Index = ({ stories }) => {
+const Index = ({ stories, page }) => {
   if (stories.length === 0) {
     return <Error statusCode={503} />
   }
 
   return (
-    <div>
-      <h1>Hacker next</h1>
+    <Layout
+      title='Hacker Next'
+      description='A Hacker News clone made with Next.js'
+    >
       <StoryList stories={stories} />
-    </div>
+
+      <footer>
+        <Link href={`/?page=${page + 1}`}>
+          <a>Next Page ({page + 1})</a>
+        </Link>
+      </footer>
+
+      <style jsx>{`
+        footer {
+          padding: 1em;
+        }
+
+        footer a {
+          text-decoration: none;
+          font-weight: bold;
+          color: black;
+        }
+      `}</style>
+    </Layout>
   )
 }
 
-Index.getInitialProps = async () => {
+Index.getInitialProps = async ({ req, res, query }) => {
   let stories
+  const page = Number(query.page) || 1
 
   try {
-    const response = await fetch('https://node-hnapi.herokuapp.com/news?page=1')
+    const response = await fetch(
+      `https://node-hnapi.herokuapp.com/news?page=${page}`
+    )
     stories = await response.json()
   } catch (error) {
     console.log(error)
     stories = []
   }
 
-  return { stories }
+  return { stories, page }
 }
 
 export default Index
